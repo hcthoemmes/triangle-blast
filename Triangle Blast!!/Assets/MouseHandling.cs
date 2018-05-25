d8cef7cdc1ddf7343b173ba19470858a8ed97e58
hcthoemmes <hcthoemmes@gmail.com>
2018-05-25 09:40:05 -0700

    Refactored MouseHandling

--- Triangle Blast!!/Assets/MouseHandling.cs
+++ Triangle Blast!!/Assets/MouseHandling.cs
@@ -4,12 +4,42 @@ using UnityEngine;
 
 public class MouseHandling : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
 
-	}
+    }
 
     RaycastHit hit;
+
+    // Update is called once per frame
+    void Update() {
+        GameObject player = GameObject.Find("Player");
+        if (Input.GetMouseButtonDown(0)) {
+            Camera camera;
+            camera = Camera.main;
+
+            float screenX = Input.mousePosition.x;
+            float screenY = Input.mousePosition.y;
+
+            Vector2 worldSizeVector = new Vector2(10, 10);
+            Vector2 pixelVector = new Vector2(screenX, screenY);
+            Vector2 screenSizeVector = new Vector2(camera.pixelHeight, camera.pixelWidth);
+
+            Vector2 worldCoordinates = NewBehaviourScript.pixelVectorToWorldVector(
+                pixelVector, worldSizeVector, screenSizeVector);
+
+            Vector2 rayDirection = NewBehaviourScript.coordinateShift(
+                worldSizeVector.x, worldSizeVector.y, worldCoordinates);
+            //Vector2 staticRayDirection = rayDirection;
+
+            onClickRaycast(rayDirection);
+            /*add a while loop for the duration of reflection that don't hit the
+              bottomWall, and then stop it*/
+            Debug.Log(Input.mousePosition);
+            Debug.Log(pixelVector + "," + worldCoordinates);
+        }
+
+    }
 
     void onClickRaycast(Vector2 v) {
         GameObject player = GameObject.Find("Player");
@@ -25,31 +55,4 @@ public class MouseHandling : MonoBehaviour {
         }
     }
 
-    // Update is called once per frame
-    void Update() {
-        GameObject player = GameObject.Find("Player");
-        if (Input.GetMouseButtonDown(0)) {
-            Camera camera;
-            camera = Camera.main;
-
-            float screenX = Input.mousePosition.x;
-            float screenY = Input.mousePosition.y;
-
-            Vector2 worldSizeVector = new Vector2(10, 10);
-            Vector2 pixelVector = new Vector2(screenX, screenY);
-            Vector2 screenSizeVector = new Vector2(camera.pixelHeight, camera.pixelWidth);
-            
-            Vector2 worldCoordinates = NewBehaviourScript.pixelVectorToWorldVector(
-                pixelVector, worldSizeVector, screenSizeVector);
-
-            Vector2 rayDirection = NewBehaviourScript.coordinateShift(
-                worldSizeVector.x, worldSizeVector.y, worldCoordinates);
-    
-            onClickRaycast(rayDirection);
-            /*add a while loop for the duration of reflection that don't hit the
-              bottomWall, and then stop it*/
-            Debug.Log(Input.mousePosition);
-            Debug.Log(pixelVector + "," + worldCoordinates);
-        }
-    }
 }
