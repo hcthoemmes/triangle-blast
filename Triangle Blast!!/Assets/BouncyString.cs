1641cc5c8ea9613521be645aeebcc4b8431c728b
hcthoemmes <hcthoemmes@gmail.com>
2018-05-25 10:23:34 -0700

    Lasers instantiated

---
+++ Triangle Blast!!/Assets/BouncyString.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncyString : MonoBehaviour {
+
+    public int id= 10;
+
+	// Use this for initialization
+	void Start () {
+        GameObject LaserString = GameObject.Find("BouncyString");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void InstantiateLaser() {
+        
+    }
+}
